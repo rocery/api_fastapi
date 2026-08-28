bcc51de21e1b0db8e72fb1966c89698156704562
rocery <sastranuralamsyah@gmail.com>
2026-08-28 11:03:17 +0700

    refactor: define API_FASTAPI_DATABASE_URL for database connection

--- app/database.py
+++ app/database.py
@@ -6,5 +6,11 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv("../.env")
 
-db_url = os.getenv("DB_HOST")
-print(f"Database URL: {db_url}")
+# Database api_fastapi
+API_FASTAPI_DATABASE_URL = (
+    f"mysql+pymysql://"
+    f"{os.getenv('API_FASTAPI_DB_USER')}:{os.getenv('API_FASTAPI_DB_PASSWORD')}"
+    f"@{os.getenv('API_FASTAPI_DB_HOST')}:{os.getenv('API_FASTAPI_DB_PORT')}"
+    f"/{os.getenv('API_FASTAPI_DB_NAME')}"
+)
+
