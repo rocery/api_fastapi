061bddcfd52a166d7c21da1e7079be5f15663be2
rocery <sastranuralamsyah@gmail.com>
2026-08-31 14:23:10 +0700

    fix: update FastAPI title and version in main.py

--- app/main.py
+++ app/main.py
@@ -4,12 +4,10 @@ from app.modules.auth.router import router as auth_router
 from app.modules.device.router import router as device_router
 from app.modules.atk.router import router as atk_router
 
-
 app = FastAPI(
-    title="Simple FastAPI API",
-    version="1.0.0"
+    title="IoT API",
+    version="0.0.1"
 )
-
 
 app.include_router(auth_router)
 app.include_router(device_router)
